0d6f4daf2ee0c58723011ec5c77910736de116a6
send-2-irfan <Irfanullah.bses19@iba-suk.edu.pk>
2024-04-04 14:45:06 +0500

    done with client side nevigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <h1>Hello Next</h1>
+      <Link href="/user">Users</Link>
     </main>
   );
 }

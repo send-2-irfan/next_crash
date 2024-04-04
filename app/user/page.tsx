b8122f81bd8ce48b1bd8e776562f895468b0197e
send-2-irfan <Irfanullah.bses19@iba-suk.edu.pk>
2024-04-04 23:46:26 +0500

    done with data fetching in ssr

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 
-const UserPage = () => {
-  return <div>UserPage</div>;
+interface Users {
+  id: number;
+  username: string;
+}
+
+const UserPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const jsonUsers: Users[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <ul>
+        {jsonUsers.map((user) => {
+          return <li key={user.id}>{user.username}</li>;
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default UserPage;
